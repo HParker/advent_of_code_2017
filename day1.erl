6e081129280321f358af597f053773a72baa0af3
Adam Hess <adamhess1991@gmail.com>
2018-01-17 11:18:14 -0800

    part 2

--- day1.erl
+++ day1.erl
@@ -1,21 +1,20 @@
 -module(day1).
 -export([answer/1]).
 
-repeats([H, H | R], Sum) ->
-    repeats([H | R], Sum + H);
-repeats([H, J | R], Sum) ->
-    repeats([J | R], Sum);
-repeats([Last | []], Sum) ->
+repeats([H | R], [H | R2], Sum) ->
+    repeats(R,R2, Sum + H);
+repeats([H | R], [H2 | R2], Sum) ->
+    repeats(R,R2, Sum);
+repeats([], [], Sum) ->
     Sum.
 
-rollover(Tokens) ->
-    [H | R] = Tokens,
-    L = lists:last(R),
-    if
-        H =:= L -> H;
-        true -> 0
-    end.
+shift_by(0, List) ->
+    List;
+shift_by(Offset, List) ->
+    [H | R] = List,
+    shift_by(Offset - 1, R ++ [H]).
 
 answer(Input) ->
     Tokens = [list_to_integer([X]) || X <- Input],
-    repeats(Tokens, 0) + rollover(Tokens).
+    Offset = length(Tokens) div 2,
+    repeats(Tokens, shift_by(Offset, Tokens), 0).
